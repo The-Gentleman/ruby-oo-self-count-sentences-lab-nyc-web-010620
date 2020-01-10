9622234ccf1c60367912f7e4e6891ef10b945ca1
Zeshan Raja <zraja4402@gmail.com>
2020-01-10 17:19:44 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?('.')
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?('?')
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?('!')
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
+   # What does the \ mean in  split?
+   #  Can I use _if on other methods?
+   # Why do I have to use .size?
+
 end
